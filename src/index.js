52280f5746fabb7dac7acdee4db40e567101394b
Kelly Cruz <kellycruz.ti@gmail.com>
2023-08-27 21:13:46 -0300

    Apollo server configs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-const { ApolloServer } = requestAnimationFrame("apollo-server")
-const typeDefs = requestAnimationFrame("./typeDefs")
-const resolvers = requestAnimationFrame("./resolvers")
+require("dotenv").config();
 
+const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server")
+
+const typeDefs = require("./typeDefs")
+const resolvers = require("./resolvers")
+
+// Database
+const db = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS,
+  name: process.env.DB_NAME,
+};
+const dbUri = `mongodb+srv://${db.user}:${db.pass}@${db.host}/${db.name}?retryWrites=true&w=majority`;
+
+const dbOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+mongoose
+  .connect(dbUri, dbOptions)
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.log("Databased failed: ", error));
+
+  //GraphQL
 const server = new ApolloServer ({ typeDefs, resolvers })
 
 server
